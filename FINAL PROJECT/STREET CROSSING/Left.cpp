e07abc1c22a9701bf1401f35b1341b70ffcb7f2a
Dai Huynh <hdaicenter@gmail.com>
2019-07-05 22:45:20 +0700

    New model

--- FINAL PROJECT/STREET CROSSING/Left.cpp
+++ FINAL PROJECT/STREET CROSSING/Left.cpp
@@ -5,16 +5,16 @@ DState Left::getCurrentState() {
 }
 
 void Left::draw(People *const peoplePtr, unsigned int x, unsigned int y, int model) {
-	switch (model) {
-	case 1: {
-		wchar_t image[3][7];
-		int w = 7, h = 3;
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				image[i][j] = ' ';
-			}
+	wchar_t image[3][7];
+	int w = 7, h = 3;
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			image[i][j] = ' ';
 		}
+	}
 
+	switch (model) {
+	case 0: {
 		image[0][2] = L'˄';
 		image[0][4] = L'˄';
 		image[1][1] = L'|';
@@ -31,6 +31,36 @@ void Left::draw(People *const peoplePtr, unsigned int x, unsigned int y, int mod
 			for (int j = 0; j < 7; j++) {
 				gotoXY(x - (w - 1) / 2 + j, y - (h - 1) / 2 + i);
 				if ((i == 0 & j == 2) || (i == 0 & j == 4) || (i == 2 & j == 0) || (i == 2 & j == 5)) {
+					randomColor(image[i][j]);
+				}
+				else {
+					wcout << image[i][j];
+				}
+			}
+		}
+		gotoXY(x, y);
+		break;
+	}
+
+	case 1: {
+		image[0][3] = L'ψ';
+		image[0][4] = L'_';
+		image[0][5] = L'ψ';
+		image[1][3] = L'(';
+		image[1][4] = L'Ж';
+		image[1][5] = L')';
+		image[2][6] = L')';
+		image[2][5] = L'┘';
+		image[2][4] = L'■';
+		image[2][3] = L'_';
+		image[2][2] = L'■';
+		image[2][1] = L'(';
+		image[2][0] = L'Ͼ';
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 7; j++) {
+				gotoXY(x - (w - 1) / 2 + j, y - (h - 1) / 2 + i);
+				if ((i == 1 & j == 3) || (i == 1 & j == 5) || (i == 0 & j == 3) || (i == 0 & j == 5)) {
 					randomColor(image[i][j]);
 				}
 				else {
--- FINAL PROJECT/STREET CROSSING/Right.cpp
+++ FINAL PROJECT/STREET CROSSING/Right.cpp
@@ -6,16 +6,16 @@ DState Right::getCurrentState() {
 }
 
 void Right::draw(People *const peoplePtr, unsigned int x, unsigned int y, int model) {
-	switch (model) {
-	case 1: {
-		wchar_t image[3][7];
-		int w = 7, h = 3;
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				image[i][j] = ' ';
-			}
+	wchar_t image[3][7];
+	int w = 7, h = 3;
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			image[i][j] = ' ';
 		}
+	}
 
+	switch (model) {
+	case 0: {
 		image[0][2] = L'˄';
 		image[0][4] = L'˄';
 		image[1][0] = L'/';
@@ -32,6 +32,36 @@ void Right::draw(People *const peoplePtr, unsigned int x, unsigned int y, int mo
 			for (int j = 0; j < 7; j++) {
 				gotoXY(x - (w - 1) / 2 + j, y - (h - 1) / 2 + i);
 				if ((i == 0 & j == 2) || (i == 0 & j == 4) || (i == 2 & j == 1) || (i == 2 & j == 6)) {
+					randomColor(image[i][j]);
+				}
+				else {
+					wcout << image[i][j];
+				}
+			}
+		}
+		gotoXY(x, y);
+		break;
+	}
+
+	case 1: {
+		image[0][1] = L'ψ';
+		image[0][2] = L'_';
+		image[0][3] = L'ψ';
+		image[1][1] = L'(';
+		image[1][2] = L'Ж';
+		image[1][3] = L')';
+		image[2][0] = L'(';
+		image[2][1] = L'└';
+		image[2][2] = L'■';
+		image[2][3] = L'_';
+		image[2][4] = L'■';
+		image[2][5] = L')';
+		image[2][6] = L'Ͽ';
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 7; j++) {
+				gotoXY(x - (w - 1) / 2 + j, y - (h - 1) / 2 + i);
+				if ((i == 1 & j == 1) || (i == 1 & j == 3) || (i == 0 & j == 1) || (i == 0 & j == 3)) {
 					randomColor(image[i][j]);
 				}
 				else {
