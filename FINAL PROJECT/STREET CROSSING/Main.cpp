960f5dde9c0f2b426b844e8ec98b97ccadbe4de6
Trần Thanh Tâm <18127268@student.hcmus.edu.vn>
2019-07-06 10:42:32 +0700

    Main

--- FINAL PROJECT/STREET CROSSING/Main.cpp
+++ FINAL PROJECT/STREET CROSSING/Main.cpp
@@ -4,12 +4,16 @@
 #include "GAME.h"
 #include "Console.h"
 
+char MOVING = ' ';
+
+void SubThread();
+
 int main() {
 	FixConsoleWindow();
 	FixSizeWindow(WIDTH, HEIGHT);
 	RemoveScrollBar();
 	ShowConsoleCursor(false);
-
+	
 	/*Car c(-20, 0);
 	char key = ' ';
 	while (true) {
@@ -25,7 +29,7 @@ int main() {
 	}*/
 
 	// Test character movement
-	_setmode(_fileno(stdout), _O_U16TEXT);
+	/*_setmode(_fileno(stdout), _O_U16TEXT);
 	People p;
 	while (true) {
 		char key;
@@ -36,8 +40,27 @@ int main() {
 			p.updatePosPeople(key);
 			p.draw();
 		}
-	}
+	}*/
+
+	SubThread();
 
 	system("pause");
 	return 0;
+}
+
+void SubThread()
+{
+	People p;
+	Train t(158, 0);
+	Car c(-20, 7);
+
+	while (true) {
+
+		//Viet cai di chuyen cua People vao day di dai
+
+		MOVING = ' ';
+		c.move(-1);
+		t.move(1);
+		Sleep(50);
+	}
 }
