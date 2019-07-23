b063b545822b62cff287104a704382d74479ba3e
tthien1920 <50990609+tthien1920@users.noreply.github.com>
2019-07-24 01:15:07 +0700

    Update River

--- FINAL PROJECT/STREET CROSSING/River.cpp
+++ FINAL PROJECT/STREET CROSSING/River.cpp
@@ -1,7 +1,8 @@
 #include "River.h"
 
 
-River::River(){
+River::River() {
+	time(&lapse);
 }
 
 River::River(int x, int y, int spd) : mX(x), mY(y), speed(spd) {
@@ -27,4 +28,35 @@ void River::setY(int y) {
 
 int River::getX() const {
 	return mX;
+}
+
+void River::drawWave(int distance) {
+	int y = mY + distance;
+	if (y < 0 || y > Y_max) {
+		return;
+	}
+	time_t now;
+	time(&now);
+
+	if (difftime(now, lapse) > 0.5) {
+		gotoXY(0, y);
+		setColor(11, 0);
+		wcout << "  .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   ";
+		gotoXY(0, y + 1);
+		setColor(1, 0);
+		wcout << "-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'";
+	}
+	else {
+		setColor(1, 0);
+		gotoXY(0, y);
+		wcout << "  .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   .-.   ";
+		gotoXY(0, y + 1);
+		setColor(11, 0);
+		wcout << "-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'   '-'";
+	}
+
+	if (difftime(now, lapse) > 1) {
+		time(&lapse);
+	}
+	setColor(7, 0);
 }
--- FINAL PROJECT/STREET CROSSING/River.h
+++ FINAL PROJECT/STREET CROSSING/River.h
@@ -3,11 +3,13 @@
 #include <string>
 #include "People.h"
 
+static time_t lapse;
+
 class River
 {
 public:
 	River();
-	River(int x, int y, int spd); 
+	River(int x, int y, int spd);
 	void set(int, int, int);
 	virtual void draw() = 0;
 	virtual void clear() = 0;
@@ -16,6 +18,7 @@ public:
 	int getX() const;
 	void setY(int y);
 	virtual ~River();
+	void drawWave(int distance);
 protected:
 	int mX, mY, speed;
 };
